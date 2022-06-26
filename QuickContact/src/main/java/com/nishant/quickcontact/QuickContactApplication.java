dc3d062f85fc91e436171394ea0f90ec26853f9b
Nishant Kumar Singh <52786028+nishu9162306344@users.noreply.github.com>
2022-06-26 17:55:25 +0530

    Add all my files

---
+++ QuickContact/src/main/java/com/nishant/quickcontact/QuickContactApplication.java
@@ -0,0 +1,13 @@
+package com.nishant.quickcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickContactApplication.class, args);
+	}
+
+}
---
+++ QuickContact/src/test/java/com/nishant/quickcontact/QuickContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishant.quickcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
